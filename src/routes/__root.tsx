16e8481911bbcef62e3f4b020c5bcf0467267952
40sinfreda <h.israel2008@gmail.com>
2026-08-26 15:48:01 +0300

    Use Tideline T SVG for apple-touch-icon on install

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -37,7 +37,7 @@ export const Route = createRootRoute({
       { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
       { rel: "stylesheet", href: appCss },
       { rel: "manifest", href: "/__grok/manifest.webmanifest" },
-      { rel: "apple-touch-icon", href: "/__grok/icon-180.png" },
+      { rel: "apple-touch-icon", href: "/tideline-icon.svg" },
     ],
   }),
   component: RootDocument,
